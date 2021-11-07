8ec829c98269e266d0f7f26ad20b43468dbe8750
Rajat Raghuwanshi <77507423+rajat10cube@users.noreply.github.com>
2021-11-06 22:28:04 -0400

    added comment2

--- LF2/lambda_function.py
+++ LF2/lambda_function.py
@@ -4,7 +4,7 @@ import requests
 from requests_aws4auth import AWS4Auth
 
 def push_to_lex(query):
-    #added comment for checking
+    #added new comment for checking
     lex = boto3.client('lex-runtime')
     print("lex client initialized")
     response = lex.post_text(
